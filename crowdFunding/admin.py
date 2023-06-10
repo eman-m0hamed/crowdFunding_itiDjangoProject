17e10b45621683c984ce3196536d1164bf3e9f32
eman-m0hamed <91141798+eman-m0hamed@users.noreply.github.com>
2023-06-10 13:30:13 +0300

    Delete crowdFunding directory

--- crowdFunding/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from crowdFunding.models import Category, Tag, myUser
-
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Tag)
-admin.site.register(myUser)
