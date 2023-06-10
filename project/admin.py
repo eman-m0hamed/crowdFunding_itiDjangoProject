9a3ce8c22c955066ac6db1171dd78f244b5d63f9
eman-m0hamed <emanmohamed73359@gmail.com>
2023-06-11 02:24:15 +0300

    projects & categories

--- project/admin.py
+++ project/admin.py
@@ -3,6 +3,5 @@ from .models import Category, Project
 
 # Register your models here.
 admin.site.register(Category)
-# admin.site.register(Tag)
 admin.site.register(Project)
-# admin.site.register(ProjectPicture)
+
